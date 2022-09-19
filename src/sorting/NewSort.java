893ab40662f913e477aaa2bebeace5e0c7b38244
mahak <mahak@10.12.2.121>
2022-09-19 09:32:50 +0530

    New Sort

---
+++ src/sorting/NewSort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {64,25,12,23,11,71};
+		int n = arr.length;
+		 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+
+	}
+
+}
